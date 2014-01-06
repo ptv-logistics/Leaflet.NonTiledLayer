18c02a28bd26a437ff3c7cf6b2399e8f1b679f20
unknown <Linda@HAL.(none)>
2014-01-06 11:35:05 +0100

    made pane configurable

--- NonTiledLayer.js
+++ NonTiledLayer.js
@@ -2,31 +2,48 @@
  * L.NonTiledLayer is an addon for leaflet which renders dynamic image overlays
  */
 L.NonTiledLayer = L.Class.extend({
-    options: {
+ 	includes: L.Mixin.Events,
+	options: {
         attribution: '',
+		opacity: 1
     },
 
     // override this method in the inherited class
     getImageUrl: function (world1, world2, width, height) {},
 
-    initialize: function (url, options) {
-        options = L.setOptions(this, options);
+    initialize: function (options) {
+        L.setOptions(this, options);
     },
 
     onAdd: function (map) {
         this._map = map;
+		
+		if(this.options.pane)
+			this._pane = this.options.pane;
+		else
+			this._pane = this._map.getPanes().overlayPane;
+	
         this._update();
+		
+		if (map.options.zoomAnimation && L.Browser.any3d) {
+			map.on('zoomanim', this._animateZoom, this);
+		}
+
         this._map.on('moveend', this._update, this);
-    },
+
+		this._pane.appendChild(this._image);
+	},
 
     onRemove: function (map) {
-        this._map.off('moveend', this._update, this);
+ 		this._pane.removeChild(this._image);
 
-        if (this._labelOverlay)
-            this._map.removeLayer(this._labelOverlay);
+		this._map.off('moveend', this._update, this);
 
-        this._labelOverlay.off('load', this._imageloaded, this);
-        this._labelOverlay = null;
+		this._pane.removeChild(this._image);
+
+		if (map.options.zoomAnimation) {
+			map.off('zoomanim', this._animateZoom, this);
+		}
     },
 
     addTo: function (map) {
@@ -34,25 +51,83 @@ L.NonTiledLayer = L.Class.extend({
         return this;
     },
 
-    // TODO remove bringToFront/bringToBack duplication from TileLayer/Path
-    bringToFront: function () {
-        if (this._labelOverlay) {
-            this._map._panes.overlayPane.appendChild(this._labelOverlay);
-        }
-        return this;
-    },
+	setOpacity: function (opacity) {
+		this.options.opacity = opacity;
+		this._updateOpacity();
+		return this;
+	},
+	
+	// TODO remove bringToFront/bringToBack duplication from TileLayer/Path
+	bringToFront: function () {
+		if (this._image) {
+			this._pane.appendChild(this._image);
+		}
+		return this;
+	},
 
-    bringToBack: function () {
-        var pane = this._map._panes.overlayPane;
-        if (this._labelOverlay) {
-            pane.insertBefore(this._labelOverlay, pane.firstChild);
-        }
-        return this;
-    },
-
+	bringToBack: function () {
+		var pane = this._pane;
+		if (this._image) {
+			pane.insertBefore(this._image, pane.firstChild);
+		}
+		return this;
+	},
+	
+	setUrl: function (url) {
+		this._url = url;
+		this._image.src = this._url;
+	},
+	
     getAttribution: function () {
         return this.options.attribution;
     },
+	
+	_initImage: function () {
+		this._image = L.DomUtil.create('img', 'leaflet-image-layer');
+
+		if (this._map.options.zoomAnimation && L.Browser.any3d) {
+			L.DomUtil.addClass(this._image, 'leaflet-zoom-animated');
+		} else {
+			L.DomUtil.addClass(this._image, 'leaflet-zoom-hide');
+		}
+
+		this._updateOpacity();
+
+		//TODO createImage util method to remove duplication
+		L.extend(this._image, {
+			galleryimg: 'no',
+			onselectstart: L.Util.falseFn,
+			onmousemove: L.Util.falseFn,
+			onload: L.bind(this._onImageLoad, this),
+			src: this._url
+		});
+	},
+
+	_animateZoom: function (e) {
+		var map = this._map,
+		    image = this._image,
+		    scale = map.getZoomScale(e.zoom),
+		    nw = this._bounds.getNorthWest(),
+		    se = this._bounds.getSouthEast(),
+
+		    topLeft = map._latLngToNewLayerPoint(nw, e.zoom, e.center),
+		    size = map._latLngToNewLayerPoint(se, e.zoom, e.center)._subtract(topLeft),
+		    origin = topLeft._add(size._multiplyBy((1 / 2) * (1 - 1 / scale)));
+
+		image.style[L.DomUtil.TRANSFORM] =
+		        L.DomUtil.getTranslateString(origin) + ' scale(' + scale + ') ';
+	},
+	
+	_reset: function () {
+		var image   = this._image,
+		    topLeft = this._map.latLngToLayerPoint(this._bounds.getNorthWest()),
+		    size = this._map.latLngToLayerPoint(this._bounds.getSouthEast())._subtract(topLeft);
+
+		L.DomUtil.setPosition(image, topLeft);
+
+		image.style.width  = size.x + 'px';
+		image.style.height = size.y + 'px';
+	},
 
     _update: function () {
         var wgsBounds = this._map.getBounds();
@@ -85,29 +160,28 @@ L.NonTiledLayer = L.Class.extend({
         var url = this.getImageUrl(world1, world2, width, height);
         var bounds = new L.LatLngBounds(world1, world2);
 
-        if (this._labelOverlay) {
+        if (this._image) 
             // update existing image hide the existing image to avoid flicker
-            L.DomUtil.setOpacity(this._labelOverlay._image, 0);
+            L.DomUtil.setOpacity(this._image, 0);
+		else 
+		  this._initImage();
+       
+	   // set new url and bounds
+       this.setUrl(url);
+       this._bounds = bounds;
 
-            // set new url and bounds
-            this._labelOverlay.setUrl(url);
-            this._labelOverlay._bounds = bounds;
-
-            // initializes the new position/transform
-            this._labelOverlay._reset();
-        } else {
-            // create the initial label layer
-            this._labelOverlay = new L.ImageOverlay(url, bounds);
-            this._map.addLayer(this._labelOverlay);
-
-            // attach to load event to reset opacity later
-            this._labelOverlay.on('load', this._imageloaded, this);
-        }
+       // initializes the new position/transform
+       this._reset();
     },
+ 
+	_onImageLoad: function () {
+       this._updateOpacity();
+	   this.fire('load');
+	},
 
-    _imageloaded: function () {
-        this._labelOverlay._updateOpacity();
-    }
+	_updateOpacity: function () {
+		L.DomUtil.setOpacity(this._image, this.options.opacity);
+	}
 });
 
 L.nonTiledLayer = function () {
