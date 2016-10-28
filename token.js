f158a6a3b8de36d9ee93f1476bc6e15a7bc47024
Marco Schmitt <Marcoschmitt@users.noreply.github.com>
2016-10-28 10:42:35 +0200

    removed token

--- token.js
+++
@@ -1,3 +0,0 @@
-// The token below is for internal use only - consider getting your own token!
-// This token is valid until 2017-01-13T17:02:54Z and was auto-generated @ 2016-10-27T21:16:30Z.
-var token = '0A6C9B37-7EE1-4AFE-8090-CE6B344C7B2C';
