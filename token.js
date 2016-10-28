c47fa44fb9135f3031105933df605b2a06d28bf6
Marco Schmitt <Marcoschmitt@users.noreply.github.com>
2016-10-28 10:43:14 +0200

    Added token.js

---
+++ token.js
@@ -0,0 +1,3 @@
+// The token below is for internal use only - consider getting your own token!
+// This token is valid until 2017-01-13T17:02:54Z and was auto-generated @ 2016-10-27T21:16:30Z.
+var token = '';
