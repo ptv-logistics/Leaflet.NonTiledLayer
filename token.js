c8ed4404095bdd5b5ab582d7e247b5101f9374f5
Oliver HEILIG (PTV Group) <oliver.heilig@ptvgroup.com>
2016-12-23 16:11:01 +0100

    using canvas, optimized zoom animation

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
 // This token is valid until 2017-01-13T17:02:54Z and was auto-generated @ 2016-10-27T21:16:30Z.
-var token = '7DBA34C1-5081-4B39-BB5D-57577430D76F';
+var token = '';
