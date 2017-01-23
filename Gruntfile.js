bd958b62074ff616caa4f27660dcadd0f53502c2
oliver.heilig@yahoo.de <oliver.heilig@yahoo.de>
2017-01-23 20:34:56 +0100

    Revert "Revert "added build""
    This reverts commit d94955068efe52a988c671f061dc730f6d1c5c7c.

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		browserify: {
+			control: {
+				src: ['src/NonTiledLayer.js', 'src/NonTiledLayer.WMS.js'],
+				dest: 'dist/NonTiledLayer-src.js',
+				options: {
+					transform: [
+						[
+							'browserify-shim',
+							{
+								'leaflet': 'global:L'
+							}
+						],
+					],
+					browserifyOptions: {
+						standalone: 'L.TileLayer.ClickableTiles'
+					}
+				}
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %> */\n\n'
+			},
+			build: {
+				src: 'dist/NonTiledLayer-src.js',
+				dest: 'dist/NonTiledLayer.js'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['browserify', 'uglify']);
+};
