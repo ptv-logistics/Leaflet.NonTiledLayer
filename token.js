168c2e264d49c16fdc8663180920856aff469a88
xserverinternet <oliver.heilig@ptvgroup.com>
2021-09-13 23:57:17 +0200

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2021-11-12T20:39:28Z and was auto-generated @ 2021-09-12T21:39:12Z.
+// This token is valid until 2021-11-12T20:39:28Z and was auto-generated @ 2021-09-13T21:39:12Z.
 var token = '874F3823-2F55-4559-806A-B18089A447F3';
