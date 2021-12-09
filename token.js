cbad5705fb977eb407088f1c6152221694929c6b
xserverinternet <oliver.heilig@ptvgroup.com>
2021-12-10 00:00:42 +0100

    Token update

---
+++ token.js
@@ -0,0 +1,3 @@
+// The token below is for internal use only - consider getting your own token!
+// This token is valid until 2022-02-04T21:39:27Z and was auto-generated @ 2021-12-09T22:39:14Z.
+var token = 'EF6009A5-CD1E-40B9-A24B-F54E1B82EA2A';
