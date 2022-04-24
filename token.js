40c48ba0fc216fac780e22bf90c2a5c2b6985e68
Oliver Heilig <oliver.heilig@yahoo.de>
2022-04-24 20:37:55 +0200

    updated layers

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2022-04-30T06:02:35Z and was auto-generated @ 2022-02-07T22:39:09Z.
-var token = '7DBFDDE5-633E-4DAA-8117-D28E128ACFFE';
+// This token is valid until 2022-06-25T05:02:28Z and was auto-generated @ 2022-04-21T21:39:19Z.
+var token = 'F49736E2-440D-4918-8B47-0C475F5A2B28';
