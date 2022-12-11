f70c23efca2eaec151d5e92bbf29141f24113530
Oliver Heilig <oliver.heilig@yahoo.de>
2022-12-11 09:33:41 +0100

    Update token.js

--- token.js
+++ token.js
@@ -1,2 +1,2 @@
 // The token below is for internal use only - consider getting your own token!
-var token = 'C2BA83B8-B432-4A74-BAE2-86106207079B';
+var token = '742DEE87-F0C7-4F71-ADF2-4BF6E3C48270';
