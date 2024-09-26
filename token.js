6db051f0f9f01e97d26b3006f18dd61755521109
Oliver Heilig <oliver.heilig@yahoo.de>
2024-09-26 09:41:33 +0200

    Update token.js

--- token.js
+++ token.js
@@ -1,2 +1,2 @@
 // This token is for demo puroposes only. Get you own free token at https://www.ptvgroup.com/en/solutions/products/ptv-xserver/ 
-var token = '23729FBD-FB95-4956-BA88-EDCEBDF6E5F8'; 
+var token = 'D1D08F9F-BE5B-41AE-8DBE-93F392BED0C7'; 
